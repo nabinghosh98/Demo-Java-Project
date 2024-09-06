a030e8abe040358c9b70b11767701e35fa9c9c58
NabinGhoshLENOVO <nabinghosh047@gmail.com>
2024-09-06 22:09:14 +0530

    my first commit to github

---
+++ src/main/java/com/example/securitydemo/MapExampleToCountTheOccurenceOfKeys.java
@@ -0,0 +1,24 @@
+package com.example.securitydemo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExampleToCountTheOccurenceOfKeys {
+
+    public static void main(String[] args){
+        Map<Character, Integer> map = new HashMap<>();
+
+        String str = "aabbaabbccdd";
+
+        for(char ch : str.toCharArray()){
+
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
+
+        for(Map.Entry<Character,Integer> entry:map.entrySet()){
+                System.out.println(entry.getKey()+"  "+ entry.getValue());
+        }
+
+        System.out.println("Size of the map is :" + map.size());
+    }
+}
